281fdbd94dcd876fc18b26390c4d2b4261e75530
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:48:54 +0900

    add RecipeEditorViewBuilder

--- MiniCookpad/Application/RecipeEditor/RecipeEditorViewBuilder.swift
+++ MiniCookpad/Application/RecipeEditor/RecipeEditorViewBuilder.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2020 kensuke-hoshikawa. All rights reserved.
 //
 
-import Foundation
+struct RecipeEditorViewBuilder {
+    static func build() -> RecipeEditorViewController {
+        let viewController = RecipeEditorViewController()
+        let interactor = RecipeEditorInteractor(imageDataStore: ImageDataStore(), recipeDataStore: RecipeDataStore())
+        let wireframe = RecipeEditorWireframe(viewController: viewController)
+        let presenter = RecipeEditorPresenter(view: viewController, interactor: interactor, wireframe: wireframe)
+        viewController.inject(presenter: presenter)
+
+        return viewController
+    }
+}
