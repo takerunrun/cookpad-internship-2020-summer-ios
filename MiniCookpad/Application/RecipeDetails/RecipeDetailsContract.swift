e479516446755fa2838c86aa217dac274d3fb3d0
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 16:02:45 +0900

    update RecipeDetailsViewProtocol

--- MiniCookpad/Application/RecipeDetails/RecipeDetailsContract.swift
+++ MiniCookpad/Application/RecipeDetails/RecipeDetailsContract.swift
@@ -1,5 +1,7 @@
 
 protocol RecipeDetailsViewProtocol: AnyObject {
+    func showRecipe(_ recipe: RecipeDetailsRecipe)
+    func showError(_ error: Error)
 }
 
 protocol RecipeDetailsPresenterProtocol: AnyObject {
