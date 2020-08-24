2de856a3bec35f46fe9e15cd3c360ee7a3eec2ea
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:07:03 +0900

    add ImagePath

---
+++ MiniCookpad/Application/RecipeEditor/ImagePath.swift
@@ -0,0 +1,13 @@
+//
+//  ImagePath.swift
+//  MiniCookpad
+//
+//  Created by admin on 2020/08/24.
+//  Copyright © 2020 kensuke-hoshikawa. All rights reserved.
+//
+
+import Foundation
+
+struct ImagePath {
+    var path: String
+}
