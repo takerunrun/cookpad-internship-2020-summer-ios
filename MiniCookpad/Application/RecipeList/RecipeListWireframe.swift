bea88d9909ad2e0e8b38801d4cd30a3efe7241e7
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 13:23:08 +0900

    add RecipeListWireframe

--- MiniCookpad/Application/RecipeList/RecipeListWireframe.swift
+++ MiniCookpad/Application/RecipeList/RecipeListWireframe.swift
@@ -1,1 +1,10 @@
 
+import UIKit
+
+class RecipeListWireframe: RecipeListWireframeProtocol {
+    private weak var viewController: UIViewController!
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+}
