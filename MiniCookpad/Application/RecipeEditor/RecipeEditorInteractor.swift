b191e33bd42a8d668da1baf91ca6602ba66ed904
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:53:31 +0900

    add validate

--- MiniCookpad/Application/RecipeEditor/RecipeEditorInteractor.swift
+++ MiniCookpad/Application/RecipeEditor/RecipeEditorInteractor.swift
@@ -21,6 +21,26 @@ class RecipeEditorInteractor: RecipeEditorInteractorProtocol {
         
     }
     
+    private static func validate(title: String?, steps: [String?], imageData: Data?) -> Result<(title: String, steps: [String], imageData: Data), RecipeEditorError> {
+        guard let imageData = imageData  else {
+            return .failure(.validationError)
+        }
+
+        guard let title = title else {
+            return .failure(.validationError)
+        }
+
+        let steps = steps.compactMap { $0 }
+        if steps.isEmpty, title.isEmpty {
+            return .failure(.validationError)
+        }
+        if containsEmoji(text: title) || (steps.map { Self.containsEmoji(text: $0) }).contains(true) {
+            return .failure(.validationError)
+        }
+
+        return .success((title: title, steps: steps, imageData: imageData))
+    }
+    
     private static func containsEmoji(text: String) -> Bool {
         let emojis = text.unicodeScalars.filter { $0.properties.isEmoji }
         return !emojis.isEmpty
