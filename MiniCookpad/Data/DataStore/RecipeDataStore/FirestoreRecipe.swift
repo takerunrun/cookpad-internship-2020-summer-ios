57786bd741dbc67375db8adcabe2d0eaa911913c
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 13:06:43 +0900

    add FirestoreRecipe

--- MiniCookpad/Data/DataStore/RecipeDataStore/FirestoreRecipe.swift
+++ MiniCookpad/Data/DataStore/RecipeDataStore/FirestoreRecipe.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+import Foundation
+// Firebase を Codable で利用できるライブラリ
+import FirebaseFirestoreSwift
+
+struct FirestoreRecipe: Codable, Equatable {
+    /// recipes/:id の id
+    @DocumentID var id: String?
+    var title: String
+    var imagePath: String
+    var steps: [String]
+    var createdAt = Date()
+}
