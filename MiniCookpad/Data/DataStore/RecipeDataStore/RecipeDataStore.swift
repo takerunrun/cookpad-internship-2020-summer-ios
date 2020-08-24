fca6a608b55077ac325ba3777f1a8266f99d93ef
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 14:46:31 +0900

    add fetchRecipe to RecipeDataStoreProtocol

--- MiniCookpad/Data/DataStore/RecipeDataStore/RecipeDataStore.swift
+++ MiniCookpad/Data/DataStore/RecipeDataStore/RecipeDataStore.swift
@@ -3,6 +3,7 @@ import Firebase
 
 protocol RecipeDataStoreProtocol {
     func fetchAllRecipes(completion: @escaping ((Result<[FirestoreRecipe], Error>) -> Void))
+    func fetchRecipe(recipeID: String, completion: @escaping ((Result<FirestoreRecipe, Error>) -> Void))
 }
 
 struct RecipeDataStore: RecipeDataStoreProtocol {
@@ -20,6 +21,17 @@ struct RecipeDataStore: RecipeDataStoreProtocol {
                 let recipe = querySnapshot!.documents
                     // 取得したデータを FirestoreRecipe に変換
                     .compactMap { try? $0.data(as: FirestoreRecipe.self)  }
+                completion(.success(recipe))
+            }
+        }
+    }
+    
+    func fetchRecipe(recipeID: String, completion: @escaping ((Result<FirestoreRecipe, Error>) -> Void)) {
+        collection.document(recipeID).getDocument { snapshot, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let recipe = try! snapshot!.data(as: FirestoreRecipe.self)!
                 completion(.success(recipe))
             }
         }
