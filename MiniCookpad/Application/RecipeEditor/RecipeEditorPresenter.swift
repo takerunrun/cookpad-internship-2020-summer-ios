c939c39482ceb712baade18a353aaeb1d1eb60c8
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:37:04 +0900

    add RecipeEditorPresenter

--- MiniCookpad/Application/RecipeEditor/RecipeEditorPresenter.swift
+++ MiniCookpad/Application/RecipeEditor/RecipeEditorPresenter.swift
@@ -5,5 +5,22 @@
 //  Created by admin on 2020/08/24.
 //  Copyright © 2020 kensuke-hoshikawa. All rights reserved.
 //
+import UIKit
 
-import Foundation
+class RecipeEditorPresenter: RecipeEditorPresenterProtocol {
+    private weak var view: RecipeEditorViewProtocol!
+    private let interactor: RecipeEditorInteractorProtocol
+    private let wireframe: RecipeEditorWireframeProtocol
+    
+    init(view: RecipeEditorViewProtocol, interactor: RecipeEditorInteractorProtocol, wireframe: RecipeEditorWireframeProtocol) {
+        self.view = view
+        self.interactor = interactor
+        self.wireframe = wireframe
+    }
+    
+    func createRecipe(title: String?, steps: [String?], image: UIImage?) {
+    }
+    
+    func close() {
+    }
+}
