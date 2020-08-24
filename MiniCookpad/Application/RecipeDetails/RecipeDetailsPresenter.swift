c78d950a747764ce96420835b0237edea40ed13a
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 15:54:26 +0900

    add recipeID to RecipeDetailsPresenter

--- MiniCookpad/Application/RecipeDetails/RecipeDetailsPresenter.swift
+++ MiniCookpad/Application/RecipeDetails/RecipeDetailsPresenter.swift
@@ -3,11 +3,13 @@ class RecipeDetailsPresenter: RecipeDetailsPresenterProtocol {
     private weak var view: RecipeDetailsViewProtocol!
     private let interactor: RecipeDetailsInteractorProtocol
     private let wireframe: RecipeDetailsWireframeProtocol
+    private let recipeID: String
     
-    init(view: RecipeDetailsViewProtocol, interactor: RecipeDetailsInteractorProtocol, wireframe: RecipeDetailsWireframeProtocol) {
+    init(view: RecipeDetailsViewProtocol, interactor: RecipeDetailsInteractorProtocol, wireframe: RecipeDetailsWireframeProtocol, recipeID: String) {
         self.view = view
         self.interactor = interactor
         self.wireframe = wireframe
+        self.recipeID = recipeID
     }
     
     func refresh() {
--- MiniCookpad/Application/RecipeDetails/RecipeDetailsViewBuilder.swift
+++ MiniCookpad/Application/RecipeDetails/RecipeDetailsViewBuilder.swift
@@ -6,7 +6,7 @@ struct RecipeDetailsViewBuilder {
         let viewController = RecipeDetailsViewController(recipeID: recipeID)
         let interactor = RecipeDetailsInteractor(recipeDataStore: RecipeDataStore())
         let wireframe = RecipeDetailsWireframe(viewController: viewController)
-        let presenter = RecipeDetailsPresenter(view: viewController, interactor: interactor, wireframe: wireframe)
+        let presenter = RecipeDetailsPresenter(view: viewController, interactor: interactor, wireframe: wireframe, recipeID: recipeID)
         viewController.inject(presenter: presenter)
 
         return viewController
