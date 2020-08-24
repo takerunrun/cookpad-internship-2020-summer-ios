1015a156140bef44144e9e31f2caa30dc40f3061
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:33:15 +0900

    update RecipeEditor protocols

--- MiniCookpad/Application/RecipeEditor/RecipeEditorContract.swift
+++ MiniCookpad/Application/RecipeEditor/RecipeEditorContract.swift
@@ -5,15 +5,23 @@
 //  Created by admin on 2020/08/24.
 //  Copyright © 2020 kensuke-hoshikawa. All rights reserved.
 //
+import UIKit
 
 protocol RecipeEditorViewProtocol: AnyObject {
+    func showValidationError()
+    func showError(_ error: Error)
+    func showComplete()
 }
 
 protocol RecipeEditorPresenterProtocol: AnyObject {
+    func createRecipe(title: String?, steps: [String?], image: UIImage?)
+    func close()
 }
 
 protocol RecipeEditorInteractorProtocol: AnyObject {
+    func createRecipe(title: String?, steps: [String?], image: UIImage?, completion: @escaping ((Result<Void, RecipeEditorError>) -> Void))
 }
 
 protocol RecipeEditorWireframeProtocol: AnyObject {
+    func close()
 }
