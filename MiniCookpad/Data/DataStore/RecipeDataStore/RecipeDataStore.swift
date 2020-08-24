0422b6a0012939ec8f541d994354919ba28eeee4
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:19:22 +0900

    update RecipeDataStoreProtocol

--- MiniCookpad/Data/DataStore/RecipeDataStore/RecipeDataStore.swift
+++ MiniCookpad/Data/DataStore/RecipeDataStore/RecipeDataStore.swift
@@ -4,6 +4,7 @@ import Firebase
 protocol RecipeDataStoreProtocol {
     func fetchAllRecipes(completion: @escaping ((Result<[FirestoreRecipe], Error>) -> Void))
     func fetchRecipe(recipeID: String, completion: @escaping ((Result<FirestoreRecipe, Error>) -> Void))
+    func createRecipe(title: String, steps: [String], imagePath: String, completion: @escaping ((Result<Void, Error>) -> Void))
 }
 
 struct RecipeDataStore: RecipeDataStoreProtocol {
@@ -33,6 +34,17 @@ struct RecipeDataStore: RecipeDataStoreProtocol {
             } else {
                 let recipe = try! snapshot!.data(as: FirestoreRecipe.self)!
                 completion(.success(recipe))
+            }
+        }
+    }
+    
+    func createRecipe(title: String, steps: [String], imagePath: String, completion: @escaping ((Result<Void, Error>) -> Void)) {
+        let recipe = FirestoreRecipe(title: title, imagePath: imagePath, steps: steps)
+        _ = try! collection.addDocument(from: recipe) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
             }
         }
     }
