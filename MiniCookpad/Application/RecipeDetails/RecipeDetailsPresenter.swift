c4cab4c68787487eb5f8780116ffc68f4160563a
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 14:52:36 +0900

    add RecipeDetailsPresenter

--- MiniCookpad/Application/RecipeDetails/RecipeDetailsPresenter.swift
+++ MiniCookpad/Application/RecipeDetails/RecipeDetailsPresenter.swift
@@ -1,1 +1,12 @@
 
+class RecipeDetailsPresenter: RecipeDetailsPresenterProtocol {
+    private weak var view: RecipeDetailsViewProtocol!
+    private let interactor: RecipeDetailsInteractorProtocol
+    private let wireframe: RecipeDetailsWireframeProtocol
+    
+    init(view: RecipeDetailsViewProtocol, interactor: RecipeDetailsInteractorProtocol, wireframe: RecipeDetailsWireframeProtocol) {
+        self.view = view
+        self.interactor = interactor
+        self.wireframe = wireframe
+    }
+}
