2c6722f7ccfd13457041f97a326e0dae2a33a39e
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 17:14:45 +0900

    add ImageDataStore

--- MiniCookpad/Data/DataStore/RecipeDataStore/ImageDataStore.swift
+++ MiniCookpad/Data/DataStore/RecipeDataStore/ImageDataStore.swift
@@ -6,8 +6,33 @@
 //  Copyright © 2020 kensuke-hoshikawa. All rights reserved.
 //
 
-import Foundation
+import FirebaseStorage
+import Firebase
 
 protocol ImageDataStoreProtocol {
     func createImage(imageData: Data, completion: @escaping ((Result<ImagePath, Error>) -> Void))
+}
+
+struct ImageDataStore: ImageDataStoreProtocol {
+    private let storageReference: StorageReference
+    
+    init(storageReference: StorageReference) {
+        self.storageReference = storageReference
+    }
+    
+    func createImage(imageData: Data, completion: @escaping ((Result<ImagePath, Error>) -> Void)) {
+        let fileName = "\(UUID()).jpg"
+        let imageRef = storageReference.child(fileName)
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        _ = imageRef.putData(imageData, metadata: metaData) { metadata, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let imagePath = ImagePath(path: fileName)
+                completion(.success(imagePath))
+            }
+        }
+    }
 }
