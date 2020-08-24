67b350ba6c17519675091ba2a73070dfa2714c53
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-24 15:03:06 +0900

    add RecipeDetailsViewBuilder

--- MiniCookpad/Application/RecipeDetails/RecipeDetailsViewBuilder.swift
+++ MiniCookpad/Application/RecipeDetails/RecipeDetailsViewBuilder.swift
@@ -1,1 +1,14 @@
 
+import UIKit
+
+struct RecipeDetailsViewBuilder {
+    static func build(with recipeID: String) -> RecipeDetailsViewController {
+        let viewController = RecipeDetailsViewController(recipeID: recipeID)
+        let interactor = RecipeDetailsInteractor(recipeDataStore: RecipeDataStore())
+        let wireframe = RecipeDetailsWireframe(viewController: viewController)
+        let presenter = RecipeDetailsPresenter(view: viewController, interactor: interactor, wireframe: wireframe)
+        viewController.inject(presenter: presenter)
+
+        return viewController
+    }
+}
